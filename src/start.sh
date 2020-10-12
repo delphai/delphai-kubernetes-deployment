5780755dc5f859055decec1a1a66f915d25f6c46
ahmed mahmoud <ahmed-delphai@ahmeds-MacBook-Air.local>
2020-10-12 15:01:05 +0200

    add acr secret

--- src/start.sh
+++ src/start.sh
@@ -1,27 +1,27 @@
 #! /usr/bin/env bash
 
+# Variables
 CHART_NAME=delphai-knative-service
 CHART_VERSION=0.1.0
 DELPHAI_ENVIROMENT=$INPUT_DELPHAI_ENVIROMENT
+
 APP_ID=$INPUT_CLIENT_ID
-PASSWORD=$INPUT_CLIENT_SECRET
+SECRET=$INPUT_CLIENT_SECRET
 TENANT_ID=$INPUT_TENANT_ID
+ACR_SECRET=$INPUT_ACR_SECRET
 REPO_NAME=$REPOSITORY_NAME
 REPO_TAG=$REPOSITORY_REF_SLUG
 IMAGE="delphai${DELPHAI_ENVIRONMENT}.azurecr.io/${REPO_NAME}:${REPO_TAG}"
 DOMAIN="delphai.red"
-apt-get install jq -y
 
-
-az login --service-principal --username $APP_ID --password $PASSWORD --tenant $TENANT_ID
+# Login and set context
+az login --service-principal --username $APP_ID --password $SECRET --tenant $TENANT_ID
 az aks get-credentials -n delphai-${DELPHAI_ENVIROMENT} -g tf-cluster 
 kubectl config current-context
-kubectl get pods --namespace delphai-boilerplate 
 
+# Helming
 export HELM_EXPERIMENTAL_OCI=1
-
-PASSWORD=$(az acr credential show --name delphai${DELPHAI_ENVIROMENT} --resource-group tf-container-registry | jq .passwords[0].value -r)
-helm registry login delphai${DELPHAI_ENVIROMENT}.azurecr.io --username delphai${DELPHAI_ENVIROMENT} --password ${PASSWORD}
+helm registry login delphai${DELPHAI_ENVIROMENT}.azurecr.io --username delphai${DELPHAI_ENVIROMENT} --password ${ACR_SECRET}
 kubectl create namespace ${REPO_NAME} --output yaml --dry-run=client | kubectl apply -f -
 kubectl patch serviceaccount default --namespace ${REPO_NAME} -p "{\"imagePullSecrets\": [{\"name\": \"acr-credentials\"}]}"
 helm repo add delphai https://delphai.github.io/helm-charts && helm repo update
@@ -29,8 +29,8 @@ helm upgrade --install --wait --atomic \
           ${REPO_NAME}-${REPO_TAG} \
           delphai/delphai-knative-service \
           --namespace=${REPO_NAME} \
-          --set image=delphaireview.azurecr.io/${REPO_NAME}:${REPO_TAG} \
+          --set image=${IMAGE} \
           --set gatewayPort=7070 \
           --set grpcPort=8080 \
           --set isPublic=true \
-          --set domain=delphai.pink
+          --set domain=${DOMAIN}
