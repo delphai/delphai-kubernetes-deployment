09dda90db17016792e0ccff2272b02d5a05b495a
ahmed mahmoud <ahmed-delphai@ahmeds-MacBook-Air.local>
2020-10-12 13:15:21 +0200

    add kubectl and azure login

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,8 @@
-FROM node:12
+FROM python:3.8
 WORKDIR /app
+RUN curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl" 
+RUN chmod +x ./kubectl 
+RUN mv /app/kubectl /usr/local/bin/kubectl 
+RUN curl -sL https://aka.ms/InstallAzureCLIDeb | bash
 COPY . /app
 ENTRYPOINT [ "/app/src/start.sh" ]
--- src/start.sh
+++ src/start.sh
@@ -1,6 +1,11 @@
 #! /usr/bin/env bash
 
-git clone https://github.com/ahmedmahmo/docker-build-push.git
-cd docker-build-push
-npm install
-node dist/index.js
+DELPHAI_ENVIROMENT=$INPUT_DELPHAI_ENVIROMENT
+APP_ID=$INPUT_CLIENT_ID
+PASSWORD=$INPUT_CLIENT_SECRET
+TENANT_ID=$INPUT_TENANT_ID
+
+az login --service-principal --username $APP_ID --password $PASSWORD --tenant $TENANT_ID
+az aks get-credentials -n delphai-${DELPHAI_ENVIROMENT} -g tf-cluster 
+kubectl config current-context
+kubectl get pods --namespace delphai-boilerplate 
