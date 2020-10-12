2e64325f37a294b3c2a2d7c8a7e058a41423ebcf
ahmed mahmoud <ahmed-delphai@ahmeds-MacBook-Air.local>
2020-10-12 13:32:37 +0200

    add helm

--- Dockerfile
+++ Dockerfile
@@ -4,5 +4,6 @@ RUN curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -
 RUN chmod +x ./kubectl 
 RUN mv /app/kubectl /usr/local/bin/kubectl 
 RUN curl -sL https://aka.ms/InstallAzureCLIDeb | bash
+RUN curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
 COPY . /app
 ENTRYPOINT [ "/app/src/start.sh" ]
--- src/start.sh
+++ src/start.sh
@@ -1,5 +1,7 @@
 #! /usr/bin/env bash
 
+CHART_NAME=delphai-knative-service
+CHART_VERSION=0.1.0
 DELPHAI_ENVIROMENT=$INPUT_DELPHAI_ENVIROMENT
 APP_ID=$INPUT_CLIENT_ID
 PASSWORD=$INPUT_CLIENT_SECRET
@@ -9,3 +11,9 @@ az login --service-principal --username $APP_ID --password $PASSWORD --tenant $T
 az aks get-credentials -n delphai-${DELPHAI_ENVIROMENT} -g tf-cluster 
 kubectl config current-context
 kubectl get pods --namespace delphai-boilerplate 
+
+PASSWORD=$(az acr credential show --name delphai${DELPHAI_ENVIRONMENT} --resource-group delphai-${DELPHAI_ENVIRONMENT} | jq .passwords[0].value -r)
+helm registry login delphai${DELPHAI_ENVIRONMENT}.azurecr.io --username delphai${DELPHAI_ENVIRONMENT} --password ${PASSWORD}
+helm chart pull delphai${DELPHAI_ENVIRONMENT}.azurecr.io/helm/${CHART_NAME}:${CHART_VERSION}
+
+
