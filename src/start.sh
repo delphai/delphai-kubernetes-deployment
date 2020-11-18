11e70592cf441d3feb61f92ed0e95e164f0ce78a
Barath Kumar <barath@delphai.com>
2020-11-18 02:06:09 +0100

    feat: production deployment

--- src/start.sh
+++ src/start.sh
@@ -32,7 +32,7 @@ if [ "${REPO_NAME}" == "delphai-ui" ]; then
     REPO_NAME="app"
 fi
 
-if [ "${REPO_SLUG}" = "master" ]; then
+if [ "${REPO_SLUG}" = "master" ] || [ "${DELPHAI_ENVIRONMENT}" == "green" ] || [ "${DELPHAI_ENVIRONMENT}" == "live" ]; then
     RELEASE_NAME=${REPO_NAME}
 else
     RELEASE_NAME=${REPO_NAME}-${REPO_SLUG}
